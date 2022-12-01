f0217b9fa00097d50f0a2dbd6e53ef6b658c0117
Afanaseva Ann <ann.afanaseva777@gmail.com>
2022-12-01 23:20:38 +0200

    01.12

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 import '../scss/styles.scss'
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+  })
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
